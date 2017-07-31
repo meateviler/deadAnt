46a3b51a2c263a9c0661058e1d292fa965f96186
meateviler <meateviler@msn.com>
2017-07-31 15:51:46 +0800

    add deadAnt

---
+++ deadAnt/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace deadAnt
+{
+	public class Kata
+	{
+		public static int DeadAntCount(string ants)
+		{
+			if (ants == null)
+				return 0;
+			var deadant = ants.ToLower().Replace("ant", string.Empty);
+
+			return Math.Max(Math.Max(deadant.Count(x => x == 'a'), deadant.Count(x => x == 'n')), deadant.Count(x => x == 't'));
+		}
+	}
+}
---
+++ deadAntTests/KataTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace deadAnt.Tests
+{
+	[TestFixture()]
+	public class KataTests
+	{
+		[TestCase(null, ExpectedResult = 0, TestName = "Input_null_shouldBe_0")]
+		[TestCase("ant ant ant ant", ExpectedResult = 0, TestName = "Input_ant ant ant ant_shouldBe_0")]
+		[TestCase("ant anantt aantnt", ExpectedResult = 2, TestName = "Input_ant anantt aantnt_shouldBe_0")]
+		[TestCase("ant ant .... a nt", ExpectedResult = 1, TestName = "Input_ant ant .... a nt_shouldBe_0")]
+		public int DeadAntCountTest(string input)
+		{
+			return Kata.DeadAntCount(input);
+		}
+	}
+}
